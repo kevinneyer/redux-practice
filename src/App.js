c30a9935e9635bfd5a0f6b073b7f1efdda16295e
kevinneyer <kevinneyer@gmail.com>
2020-10-21 16:09:54 -0400

    implemented basic counter using redux

--- src/App.js
+++ src/App.js
@@ -1,56 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+import React, { useState } from 'react';
 import './App.css';
+import { useSelector, useDispatch } from 'react-redux'
+import { increment } from './actions'
+import { decrement } from './actions'
 
 function App() {
+
+  const counter = useSelector(state => state)
+  const dispatch = useDispatch()
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+    <div className='App'>
+      <h1>Counter</h1>
+        <div>
+          <span className='count'> {counter} </span><br/>
+          <button onClick={ () => dispatch(decrement()) }>-</button>
+          <button onClick={ () => dispatch(increment()) }>+</button>
+        </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,17 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import store from './app/store';
-import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import counterReducer from './reducers/counter'
+
+
+const store = createStore( 
+  counterReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() 
+)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
+<React.StrictMode>
+    <Provider store={ store }>
       <App />
     </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  
+ 
+</React.StrictMode>,
+document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
